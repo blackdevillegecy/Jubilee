15d6e8468b3dd18343e5350f4e47b3cee184feda
atronos <slenderblendertender@gmail.com>
2023-01-10 19:49:22 +0530

    aj

--- jubilee/pages/about.jsx
+++ jubilee/pages/about.jsx
@@ -5,6 +5,13 @@ function About() {
     return (
         <div className={styles.about} >
             <Navbar />
+            <div>
+                <p><br/><br/><br/><br/><br/><br/>Welcome to JUBILEE., where we specialize in creating extraordinary events that leave a lasting impression. As a full-service event planning company, we offer a wide range of services to meet all of your event needs. Our team of experienced professionals has a diverse range of skills and expertise, ensuring that no detail is overlooked. From corporate events and conferences to weddings and social gatherings, we have the knowledge and resources to make your event a success.
+
+We pride ourselves on our ability to bring your vision to life, no matter the occasion. Whether you're looking for a lavish wedding, a corporate gala, or a intimate social gathering, we have the expertise and resources to make it happen. Our services include venue selection, decor, catering, entertainment, and more. We take the stress out of event planning and help you create a truly unforgettable experience.
+
+At JUBILEE., customer satisfaction is our top priority. We work closely with our clients to understand their unique needs and preferences, and we strive to exceed their expectations every step of the way. If you're ready to start planning your dream event, we're here to help. Contact us today to get started!</p>
+            </div>
         </div>
     )
 }
--- jubilee/pages/index.js
+++ jubilee/pages/index.js
@@ -1,5 +1,5 @@
 import Gallery from "../components/Gallery/Gallery";
-import Navbar from "../components/Navbar/Navbar";
+import Navbar from "../components/Navbar/Navbar"
 import Services from "../components/Services/Services";
 import styles from "../styles/Home.module.css";
 
@@ -10,9 +10,10 @@ function Home() {
     </div>
     <Navbar />
     <div className={styles.home_elements}>
-      <div className={styles.heading}>Magic is Waiting for You</div>
-      <p className={styles.para}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas enim lacus, lacinia ornare quam id, vehicula ornare augue. Vestibulum ante ipsum primis vehicula ornare augue. Vestibulum ante ipsum</p>
-      <div className={styles.button}>LOREM IPSUM</div>
+      <div className={styles.heading}>JUBILEE.</div>
+      <div className={styles.heading2}>Turn your event into an experience</div> 
+      <p className={styles.para}>Are you overwhelmed by the thought of planning your next big event? Let us take the stress off your shoulders and help you create a seamless, unforgettable experience. We pride ourselves on our ability to bring your vision to life and deliver a truly memorable event for you and your guests.</p>
+      <div className={styles.button}>BOOK NOW!8</div>
     </div>
     <Services />
     {/* <Gallery /> */}
